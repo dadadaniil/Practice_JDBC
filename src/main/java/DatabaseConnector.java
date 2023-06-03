6672a268e2796b4faac2656b6c7b569ce8e78f63
Dadadaniil <den382505@gmail.com>
2023-06-03 11:28:58 +0300

    харош, ультанул(сделал)

--- src/main/java/DatabaseConnector.java
+++ src/main/java/DatabaseConnector.java
@@ -1,99 +1,32 @@
 import java.sql.*;
-import java.util.ArrayList;
 
 public class DatabaseConnector {
     private static final String USERNAME = "user";
     private static final String PASSWORD = "root";
     private static final String DB_URL = "jdbc:mysql://localhost:3306/my_database";
-    private static Connection conn;
+    public static final String FREQUENCIES_TABLE = "frequencies";
+    public static final String COORDINATES_TABLE = "coordinates";
 
-    static {
+    private Connection conn;
+
+    public DatabaseConnector() {
         try {
             conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error connecting to the database.", e);
         }
     }
 
-    public DatabaseConnector() throws SQLException {
+    public Connection getConnection() {
+        return conn;
     }
 
-    public static void main(String[] args) {
+    public boolean isConnected() {
         try {
-
-            DatabaseConnector dbConnector = new DatabaseConnector();
-            if (dbConnector.connect()) {
-                System.out.println("Connected to the server.");
-            }
-
-            Statement stmt = conn.createStatement();
-
-            ResultSet rs = stmt.executeQuery("SELECT * FROM coordinates");
-
-            ArrayList<LenNumPair> listOfPairs = new ArrayList<>();
-
-            while (rs.next()) {
-                LenNumPair pair2 = new LenNumPair(rs.getDouble("x1"), rs.getDouble("x2"));
-
-                boolean isPairFound = false;
-
-                System.out.println(rs.getInt("x1") + " " + rs.getInt("x2") + " " + pair2);
-
-                for (LenNumPair pair : listOfPairs) {
-                    if (pair.getLen() == pair2.getLen()) {
-                        isPairFound = true;
-                        pair.setNum(pair.getNum() + 1);
-                        break;
-                    }
-                }
-                if (!isPairFound) {
-                    listOfPairs.add(pair2);
-                }
-
-            }
-            System.out.println();
-            System.out.println(listOfPairs);
-            System.out.println();
-
-            String clearTableSQL = "DELETE FROM frequencies";
-            stmt.executeUpdate(clearTableSQL);
-
-            for (LenNumPair pair : listOfPairs) {
-                String insertSQL = "INSERT INTO frequencies (len, num) VALUES (?, ?)";
-                PreparedStatement pstmt = conn.prepareStatement(insertSQL);
-                pstmt.setInt(1, pair.getLen());
-                pstmt.setInt(2, pair.getNum());
-                pstmt.executeUpdate();
-            }
-
-            ResultSet rs2=stmt.executeQuery("SELECT *FROM frequencies  WHERE len>num");
-            while (rs2.next()){
-                    System.out.println("len="+rs2.getInt("len")+" is greater then num="+rs2.getInt("num"));
-            }
-
-
+            return conn != null && !conn.isClosed();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error checking connection status.", e);
         }
-    }
-
-    private boolean establishConnection(String dbURL) {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(dbURL, USERNAME, PASSWORD);
-        } catch (ClassNotFoundException e) {
-            System.out.println("JDBC Driver not found.");
-            e.printStackTrace();
-            return false;
-        } catch (SQLException e) {
-            System.out.println("Error connecting to the database.");
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
-
-    public boolean connect() {
-        return establishConnection(DB_URL);
     }
 }
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,66 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class Main {
 
+    private static final String NUM = "num";
+    private static final String LEN = "len";
+    private static final String SELECT_COORDINATES_QUERY = "SELECT * FROM " + DatabaseConnector.COORDINATES_TABLE;
+    private static final String DELETE_FREQUENCIES_QUERY = "DELETE FROM " + DatabaseConnector.FREQUENCIES_TABLE;
+    private static final String INSERT_FREQUENCIES_QUERY = "INSERT INTO " + DatabaseConnector.FREQUENCIES_TABLE + " (" + LEN + ", " + NUM + ") VALUES (?, ?)";
+    private static final String SELECT_FREQUENCIES_QUERY = "SELECT * FROM " + DatabaseConnector.FREQUENCIES_TABLE + " WHERE len > num ORDER BY len ASC";
+
+    public static void main(String[] args) {
+        try {
+            DatabaseConnector dbConnector = new DatabaseConnector();
+            if (!dbConnector.isConnected()) {
+                System.out.println("Failed to connect to the server.");
+                return;
+            }
+            System.out.println("Connected to the server.");
+            System.out.println();
+
+            Connection conn = dbConnector.getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(SELECT_COORDINATES_QUERY);
+            List<LenNumPair> pairsList = new ArrayList<>();
+
+            while (rs.next()) {
+                LenNumPair newPair = new LenNumPair(rs.getDouble("x1"), rs.getDouble("x2"));
+
+                Optional<LenNumPair> existingPairOpt = pairsList.stream()
+                        .filter(pair -> pair.getLen() == newPair.getLen())
+                        .findFirst();
+
+                if (existingPairOpt.isPresent()) {
+                    LenNumPair existingPair = existingPairOpt.get();
+                    existingPair.setNum(existingPair.getNum() + 1);
+                } else {
+                    pairsList.add(newPair);
+                }
+                System.out.println(newPair);
+            }
+
+            System.out.println();
+            stmt.executeUpdate(DELETE_FREQUENCIES_QUERY);
+
+            for (LenNumPair pair : pairsList) {
+                PreparedStatement pstmt = conn.prepareStatement(INSERT_FREQUENCIES_QUERY);
+                pstmt.setInt(1, pair.getLen());
+                pstmt.setInt(2, pair.getNum());
+                pstmt.executeUpdate();
+            }
+
+            ResultSet rs2 = stmt.executeQuery(SELECT_FREQUENCIES_QUERY);
+
+            while (rs2.next()) {
+                System.out.println(LEN + " = " + rs2.getInt(LEN) + " is greater than num = " + rs2.getInt(NUM));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error executing SQL.", e);
+        }
+    }
 }
